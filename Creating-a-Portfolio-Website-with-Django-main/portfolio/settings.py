392cac76b0acb30b11e5f3a4f1182cc1f9671eda
Bethuel Moukangwe <93348615+BETHUELTHIPE@users.noreply.github.com>
2024-10-27 23:42:44 +0200

    Update settings.py

--- Creating-a-Portfolio-Website-with-Django-main/portfolio/settings.py
+++ Creating-a-Portfolio-Website-with-Django-main/portfolio/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = "SECRET_KEY"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [bethuel-portfoliowebsite-xbbw.vercel.app
-]
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
