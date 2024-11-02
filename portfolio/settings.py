e55b27fef0c50fc27622cdbce89ac3d97b8b19b6
Bethuel Moukangwe <93348615+BETHUELTHIPE@users.noreply.github.com>
2024-11-02 03:49:54 +0200

    Update settings.py
    AWS S3 storage created

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -98,3 +98,14 @@ STATICFILES_DIRS = [BASE_DIR / "static"]  # additional static files directory
 
 # Default auto field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AWS_ACCESS_KEY_ID = 'AKnsbhjbhuXGGvbhJHBUjxFIHKJAhguyGJJHhyujnXkm '
+AWS_SECRET_ACCESS_KEY = 'nJXNJNJDOIJXKmkMXKkmxsbshhhshbshsbb hyjmann'    
+'
+AWS_STORAGE_BUCKET_NAME = 'bethuel-bebsites-bucket'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
